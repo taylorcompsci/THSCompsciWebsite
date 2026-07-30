b41f0f326082a1947b13c68c99474d0635e7e835
Jaiden Khosla <74748528+JaidenKhosla@users.noreply.github.com>
2026-07-30 11:49:34 -0500

    Working on calendar

---
+++ app/about/page.tsx
@@ -0,0 +1,10 @@
+import EventCalendar from "@/util/About/EventCalendar";
+
+export default function About()
+{
+    return (
+        <div className="mt-10">
+            <EventCalendar/>
+        </div>
+    );
+}
