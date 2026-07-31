4af0c65052a05a6ebb514e486d001ad31dc2ecbc
Jaiden Khosla <74748528+JaidenKhosla@users.noreply.github.com>
2026-07-31 01:28:58 -0500

    Google Calendar API Sucks. Improved EventCalendarAction

--- util/About/EventCalendar.tsx
+++ util/About/EventCalendar.tsx
@@ -1,6 +1,8 @@
 "use client"
 
 import Calendar, { TileArgs } from "react-calendar";
+import { useEffect, useState } from "react";
+import { getCalendarEntries, type CalendarEntry } from "@/actions/CalendarAction";
 
 function tileContent({view, date}: TileArgs)
 {
@@ -13,7 +15,20 @@ function tileContent({view, date}: TileArgs)
 
 export default function EventCalendar()
 {
+
+    const [ useDate, setDate ] = useState<Date>(new Date());
+    const [ useEntries, setEntries ] = useState<CalendarEntry[]>([]);
+
+    useEffect(()=>{
+        getCalendarEntries().then((events)=>{setEntries(events); console.log(events)}).catch(err=> { throw err})
+    }, []);
+
     return (
-        <Calendar tileContent={tileContent} tileClassName="calendar-tile"/>
+        <div className="flex">
+            <div>
+                Event Model
+            </div>
+            <Calendar tileContent={tileContent} tileClassName="calendar-tile"/>
+        </div>
     )
 }
